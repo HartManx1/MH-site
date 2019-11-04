7a5cbaf78d61c964f9f118b6e2cb4be828d5dd5a
Martin Hartman <hartmanx1@wp.pl>
2019-11-04 20:33:10 +0100

    v2.1

---
+++ build/precache-manifest.530428ff2b071227e7da648890102f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbf92d6f0ab38cb95365673998abc27",
+    "url": "/MH-site/index.html"
+  },
+  {
+    "revision": "d54d08567af58476f266",
+    "url": "/MH-site/static/css/main.501df9a4.chunk.css"
+  },
+  {
+    "revision": "dfdc4a100e877f2b3cf1",
+    "url": "/MH-site/static/js/2.20420a31.chunk.js"
+  },
+  {
+    "revision": "d54d08567af58476f266",
+    "url": "/MH-site/static/js/main.e8afe516.chunk.js"
+  },
+  {
+    "revision": "a5002e78caa60e48bbf9",
+    "url": "/MH-site/static/js/runtime-main.b7fd0917.js"
+  },
+  {
+    "revision": "8e1973512cf65cb9f218b951f777bae1",
+    "url": "/MH-site/static/media/avatar.8e197351.jpg"
+  },
+  {
+    "revision": "30bd974d76d0cff33d365f34422dc309",
+    "url": "/MH-site/static/media/certificate_kodilla2019.30bd974d.jpg"
+  }
+]);
